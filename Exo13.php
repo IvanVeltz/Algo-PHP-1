06155bbdf9838f9c6692ddd4c50777bf2f429845
Ivan Veltz <ivan.veltz@live.fr>
2025-03-12 16:53:16 +0100

    exo13 modif

--- Exo13.php
+++ Exo13.php
@@ -8,10 +8,7 @@ coefficient). Elle devra être affichée avec 2 décimales.</p>
 <?php 
 
 $notes = [10 ,12 ,8 ,19, 3, 16, 11, 13, 9];
-echo "Les notes obtenus sont : ";
-foreach($notes as $note){
-    echo "$note ";
-}
+echo "Les notes obtenus sont : ".implode(" ",$notes);
 $total = array_sum($notes);
 $moyenne =  round($total/count($notes),2);
 echo "\nSa moyenne générale est donc de : $moyenne";
