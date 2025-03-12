06cccc52b7d4ec1e81dbbe840431ba455e6fce6c
Ivan Veltz <ivan.veltz@live.fr>
2025-03-12 11:14:42 +0100

    Exo4

--- Exo4.php
+++ Exo4.php
@@ -4,3 +4,20 @@
 
 <h2>Résultat</h2>
 
+<?php
+function palindrome($phrase) {
+    // Enlever les espaces et mettres en minuscule
+    $phrTest = strtolower(str_replace(" ","",$phrase));
+
+    // Comparer $phrTest avec son inverse
+    if ($phrTest == strrev($phrTest)) {
+        echo 'La phrase "'.$phrase.'" est un palidrome"'; 
+    } else {
+        echo 'La phrase "'.$phrase.'" n\'est un palidrome"';
+    }
+
+}
+
+$phrase = 'Engage le jeu que je le gagne';
+
+echo palindrome($phrase);
