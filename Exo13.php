a916b44135d6e7aa966efa22d932cdf448a0273f
Ivan Veltz <ivan.veltz@live.fr>
2025-03-12 16:28:13 +0100

    Exo 13

---
+++ Exo13.php
@@ -0,0 +1,19 @@
+<h1>Exercice 13</h1>
+
+<p>Calculer la moyenne générale d'un élève dont les notes sont renseignées dans un tableau (pas de 
+coefficient). Elle devra être affichée avec 2 décimales.</p>
+
+<h2>Résultat</h2>
+
+<?php 
+
+$notes = [10 ,12 ,8 ,19, 3, 16, 11, 13, 9];
+echo "Les notes obtenus sont : ";
+$total = 0;
+foreach($notes as $note){
+    $total += $note;
+    echo "$note ";
+}
+
+$moyenne =  round($total/count($notes),2);
+echo "\nSa moyenne générale est donc de : $moyenne";
