b3ceb59822882a441360706a0ffdf67814ea62ef
Ivan Veltz <ivan.veltz@live.fr>
2025-03-12 11:21:48 +0100

    Exo5

--- Exo5.php
+++ Exo5.php
@@ -1,4 +1,4 @@
-<h1>Exercice 4</h1>
+<h1>Exercice 5</h1>
 
 <p>Ecrire un algorithme qui déclare une valeur en francs et qui la convertit en euros. 
 Attention, la valeur générée devra être arrondie à 2 décimales.</p>
